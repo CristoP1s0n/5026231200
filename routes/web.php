c0bab4e80de9c083809b1556cfb7cd7e693b8e95
Crit0_ <162570032+CristoP1s0n@users.noreply.github.com>
2025-06-03 23:15:11 +0700

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,10 @@
-<?php
-
+// routes/web.php
+use App\Http\Controllers\PegawaiController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk CRUD Pegawai
+Route::resource('pegawai', PegawaiController::class);
